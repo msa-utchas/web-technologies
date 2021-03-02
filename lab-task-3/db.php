22750fbfc7edc0350693c7bf21b2bf9070d54989
MD SAKIBUL ALAM UTCHAS <msa-utchas@users.noreply.github.com>
2021-03-02 16:21:46 +0600

    lab3

--- lab-task-3/db.php
+++ lab-task-3/db.php
@@ -10,14 +10,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error);}
 }
 
-
-// $user = " ihsan ";
-// $email ="ihsan@gmail.com";
-// $username = " aqib ";
-// $password = "1234";
-// $gender = "male";
-// $birthday = "23 feb 1998";
-
 function value($conn,$user,$email,$username,$password,$gender,$birthday)
 {
     $sql = "INSERT INTO user (user,email,username,password,gender,birthday)
